2127942952e77738b04acac0b40e2201860f43c9
Anthony Kelley <anthonymkelley@gmail.com>
2017-12-01 15:42:26 -0700

    initial commit

---
+++ db/migrate/20171201215412_create_locations.rb
@@ -0,0 +1,11 @@
+class CreateLocations < ActiveRecord::Migration[5.1]
+  def change
+    create_table :locations do |t|
+      t.string :address, null: false
+      t.integer :priority, null: false
+      t.belongs_to :trip, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
