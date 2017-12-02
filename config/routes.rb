fedf21e532109f32845065419386fea119e9d54d
cgregs32 <cgregs32@gmail.com>
2017-12-02 16:34:11 -0700

    finalized navigation, styling to trips index page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :trips
   resources :locations
 
+  # get '/trip/:id/location/:id', to: 'locations#new', as: 'new_location'
   get '/trip/:id/find_location/:id', to: 'trips#find_location', as: 'find_location'
   patch '/trip/:id/add_location/', to: 'trips#add_location', as: 'add_location'
   patch '/trip/:id/remove_location/:trip_id', to: 'trips#remove_from_trip', as: 'remove_location'
